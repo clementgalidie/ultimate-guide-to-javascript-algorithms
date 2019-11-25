7401906d3f598716b5905f5f7f8f665b0846a661
Clement Galidie <galidie.clement@gmail.com>
2019-11-25 19:46:04 +0100

    add hamming distance forEach loop

--- Beginner/hammingDistance/index-SOLUTION.js
+++ Beginner/hammingDistance/index-SOLUTION.js
@@ -14,3 +14,23 @@ function hammingDistance(stringA, stringB) {
         throw new Error('Strings do not have equal length')
     }
 }
+
+// USING FOREACH LOOP
+function hammingDistanceForEach(stringA, stringB) {
+  if (stringA.length !== stringB.length) {
+    console.error('The two strings should have the same length.')
+    return
+  }
+
+  let diffCounter = 0
+  const arrStringA = [...stringA.toLowerCase()]
+  const arrStringB = [...stringB.toLowerCase()]
+
+  arrStringA.forEach((value, index) => {
+    if (arrStringA[index] !== arrStringB[index]) {
+      diffCounter++
+    }
+  })
+
+  return diffCounter
+}
