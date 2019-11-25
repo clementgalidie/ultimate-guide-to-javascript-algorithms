9b0706e41d63a41a502e2273ed6a8659afc2a352
Clement Galidie <galidie.clement@gmail.com>
2019-11-25 12:38:58 +0100

    fix code syntax

--- Beginner/palindromeChecker/index-SOLUTION.js
+++ Beginner/palindromeChecker/index-SOLUTION.js
@@ -1,39 +1,39 @@
 // AN INTUITIVE APPROACH
 
-function palindromeChecker (text) {
+function palindromeChecker(text) {
 
-  var reversedText = text.toLowerCase()
+    var reversedText = text.toLowerCase()
         .split('').reverse().join('')
 
-  return text === reversedText
+    return text === reversedText
 }
 
 
 // LOOPING THROUGH AND COMPARING CHARACTERS
 
 
-function palindromeChecker (text) {
-  let charArray = text.toLowerCase().split('')
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
 
-  let result = charArray.every((letter, index) => {
-    return letter === charArray[charArray.length - index - 1]
-  })
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index - 1];
+    })
 
-  return result
+    return result
 }
 
 // LOOPING THROUGH AND COMPARING CHARACTERS(OPTIMIZED)
 
 
-function palindromeChecker (text) {
-  const textLowered = text.toLowerCase()
-  const textLen = text.length
+function palindromeChecker(text) {
+    const textLowered = text.toLowerCase()
+    const textLen = text.length
 
-  for (let i = 0; i < textLen / 2; i++) {
-    if (textLowered[i] !== textLowered[textLen - i - 1]) {
-      return false
+    for (let i = 0; i < textLen / 2; i++) {
+        if (textLowered[i] !== textLowered[textLen - i - 1]) {
+            return false
+        }
     }
-  }
 
-  return true
+    return true
 }
